e61462016c91ce7b64d6f9b78a2918d6cbf547f4
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-14 11:49:40 +0500

    add register router in urls app materials

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,15 @@
 from materials.apps import MaterialsConfig
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from materials.views import CourseViewSet
 
 app_name = MaterialsConfig.name
+
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename="courses")
+
 
 urlpatterns = [
 
