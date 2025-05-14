06220e546b706475502b10ed373beb39197cac1e
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-14 14:36:35 +0500

    add urls for model Course, Lesson in app materials

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,8 @@ from materials.apps import MaterialsConfig
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MaterialsConfig.name
 
@@ -12,5 +13,12 @@ router.register(r'courses', CourseViewSet, basename="courses")
 
 
 urlpatterns = [
+    path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path("lessons/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson-detail"),
+    path("'lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson-update"),
+    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson-delete"),
 
-]
+    path("courses/<int:course_id>/lessons/", LessonListAPIView.as_view(), name='course-lessons'),
+
+] + router.urls
