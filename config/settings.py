a970a293b6d479898c12cc865f4badc7ddc878ae
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-14 09:53:17 +0500

    update setting in config

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
+    "materials",
+
 ]
 
 MIDDLEWARE = [
@@ -127,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
