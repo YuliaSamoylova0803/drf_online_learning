1a14c675e141ab6cf5c37c24fcb8ef59619c3841
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-14 09:16:34 +0500

    add model User in admin for app users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
 # Register your models here.
+@admin.register
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "phone", "city", "avatar", "tg_nick")
