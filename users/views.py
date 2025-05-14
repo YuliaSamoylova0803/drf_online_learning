62ef94dc0f0a567cfecd1e7c8843799d05a0f504
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-14 15:30:11 +0500

    add views in app users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from os import getenv
 
-# Create your views here.
+from django.core.serializers import serialize
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserProfileSerializer
+from rest_framework.response import Response
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        """Возвращает текущего пользователя"""
+        return self.request.user
+
+    def list(self, request, *args, **kwargs):
+        """Показывает профиль текущего пользователя"""
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
