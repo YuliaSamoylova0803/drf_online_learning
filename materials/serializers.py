02d2b89b0b98d9829cc88fead7a643d9a2724fb7
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-18 15:32:24 +0500

    update serializers in materials: add get_lessons count

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+
+from users.models import Payment
 from .models import Course, Lesson
 
 
@@ -9,8 +12,46 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class CourseDetailSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "title",
+            "preview",
+            "description",
+            "created_at",
+            "updated_at",
+            "lessons_count",  # Добавляем новое поле в список полей
+        ]
+        read_only_fields = ["created_at", "updated_at", "lessons_count"]  # Делаем поле только для чтения
+
+    def get_lessons_count(self,instance):
+        """Возвращает количество уроков в курсе"""
+        return instance.lessons.count() # Используем related_name 'lessons' из модели Lesson
+
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    paid_course = CourseSerializer(read_only=True)
+    paid_lesson = LessonSerializer(read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = [
+            'id',
+            'user',
+            'date_of_payment',
+            'paid_course',
+            'paid_lesson',
+            'amount',
+            'payment_method'
+        ]
+        read_only_fields = ['payment_date']
