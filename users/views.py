4ab3ba7f94960d65de81c26a9ffbccfffbfcaac3
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-19 11:43:02 +0500

    update views in users

--- users/views.py
+++ users/views.py
@@ -2,14 +2,17 @@ from os import getenv
 
 from django.core.serializers import serialize
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
-from .models import User
-from .serializers import UserProfileSerializer
+from rest_framework.filters import OrderingFilter
+
+from .models import User, Payment
+from .serializers import UserProfileSerializer, PaymentHistorySerializer, PaymentSerializer
 from rest_framework.response import Response
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().prefetch_related("payments")
     serializer_class = UserProfileSerializer
 
     def get_object(self):
@@ -21,3 +24,20 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]  # Бэкенд для обработки фильтра
+    # Поля, по которым можно фильтровать
+    filterset_fields = (
+        "paid_course",  # Фильтр по ID курса
+        "paid_lesson",  # Фильтр по ID урока
+        "payment_method",  # Фильтр по способу оплаты (cash/transfer)
+    )
+    # Поля, по которым можно сортировать
+    ordering_fields = ["date_of_payment", "amount"]
+
+    # Сортировка по умолчанию (если не указан параметр `ordering`)
+    ordering = ["-date_of_payment"]  # Новые платежи сначала
