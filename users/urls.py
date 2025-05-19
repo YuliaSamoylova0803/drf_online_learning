816a7767768708ee1189ace62c27234b7ea9cf45
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-19 11:42:12 +0500

    update urls in users

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
+from users.serializers import PaymentSerializer
 from users.apps import UsersConfig
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet
-
+from .views import UserProfileViewSet, PaymentViewSet
 
 app_name = UsersConfig.name
 
 
 router = DefaultRouter()
 router.register(r"", UserProfileViewSet, basename="user-profile")
+router.register(r"payments", PaymentViewSet, basename='payments')
 
 urlpatterns = router.urls
