7df4fc6d7b34614aa6b56f9ed47c242e84e42be2
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-20 16:39:30 +0500

    update urls in users

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,20 @@
-from users.serializers import PaymentSerializer
 from users.apps import UsersConfig
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet, PaymentViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
-
 
 router = DefaultRouter()
 router.register(r"", UserProfileViewSet, basename="user-profile")
 router.register(r"payments", PaymentViewSet, basename='payments')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+] + router.urls
