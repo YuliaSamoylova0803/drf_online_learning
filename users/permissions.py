d532644539895403fc148ccdb9e144927f3cfd25
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-21 15:33:36 +0500

    update permissions in users app

--- users/permissions.py
+++ users/permissions.py
@@ -1,6 +1,17 @@
 from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
 
 class IsModerPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moders").exists()
+
+
+class IsOwnerOrStaff(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+
+        return request.user == view.get_object().owner
