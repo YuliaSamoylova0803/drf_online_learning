af32b57637a5cc346fe7465f3b8b14b3c5eeda5d
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-21 16:45:45 +0500

    update views in users

--- users/views.py
+++ users/views.py
@@ -1,17 +1,25 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-
+from .permissions import IsOwnerOrStaff
 from .models import User, Payment
-from .serializers import UserProfileSerializer, PaymentHistorySerializer, PaymentSerializer, UserSerializer
+from .serializers import (
+    UserProfileSerializer,
+    PaymentHistorySerializer,
+    PaymentSerializer,
+    UserSerializer,
+    OtherUserProfileSerializer,
+)
 from rest_framework.response import Response
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().prefetch_related("payments")
     serializer_class = UserProfileSerializer
+    permission_classes = [IsOwnerOrStaff]
 
     def get_object(self):
         """Возвращает текущего пользователя"""
@@ -23,11 +31,29 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            if self.kwargs.get("pk") == str(self.request.user.pk):
+                return UserProfileSerializer  # Полная информация для своего профиля
+            return (
+                OtherUserProfileSerializer  # Ограниченная информация для чужого профиля
+            )
+        return UserProfileSerializer
+
+    @action(detail=False, methods=["get"])
+    def me(self, request):
+        """Эндпоинт для получения текущего пользователя"""
+        serializer = UserProfileSerializer(request.user, context={"request": request})
+        return Response(serializer.data)
+
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = [DjangoFilterBackend, OrderingFilter]  # Бэкенд для обработки фильтра
+    filter_backends = [
+        DjangoFilterBackend,
+        OrderingFilter,
+    ]  # Бэкенд для обработки фильтра
     # Поля, по которым можно фильтровать
     filterset_fields = (
         "paid_course",  # Фильтр по ID курса
