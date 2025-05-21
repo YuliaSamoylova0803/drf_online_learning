c3b24c28314c7a1ed3ab39fb482d8e84d31ce5a0
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-21 09:41:28 +0500

    update urls in config

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,6 @@ from config import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("materials.urls", namespace="materials")),
-    path("profile/", include("users.urls")),
     path("users/", include("users.urls", namespace="users")),
 ]
 if settings.DEBUG:
