37cab533dcab427c858e2ef61ff6ec46c64c2240
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-25 00:46:29 +0500

    add serializer for model Subscribe in serializers in materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,14 +1,22 @@
 from dataclasses import field
 
 from rest_framework import serializers
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscribe
 from .validators import StrictYouTubeLinkValidator
 
 class CourseSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_is_subscribed(self, obj):
+        """Проверяет, подписан ли текущий пользователь на курс"""
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Subscribe.objects.filter(user=user, course=obj).exists()
+        return False
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -24,6 +32,7 @@ class LessonSerializer(serializers.ModelSerializer):
 class CourseDetailSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
@@ -41,6 +50,7 @@ class CourseDetailSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
             "lessons_count",
+            "is_subscribed"
         ]  # Делаем поле только для чтения
 
     def get_lessons_count(self, instance):
@@ -48,3 +58,15 @@ class CourseDetailSerializer(serializers.ModelSerializer):
         return (
             instance.lessons.count()
         )  # Используем related_name 'lessons' из модели Lesson
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Subscribe.objects.filter(user=user, course=obj).exists()
+        return False
+
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscribe
+        fields = '__all__'
