837a5fc18db6ed2a0d73592eabc964b6aa75f3e3
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-03 15:15:56 +0500

    update file setting in config

--- config/settings.py
+++ config/settings.py
@@ -159,3 +159,12 @@ SIMPLE_JWT = {
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+# Celery Configuration Options
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+#URL-адрес брокера сообщений
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
