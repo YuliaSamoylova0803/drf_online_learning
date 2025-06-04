7b62795e6ea8f7bf10470bb70567a5cdbcb1e840
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-05 01:05:45 +0500

    update celery in config

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -15,3 +16,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+# Самая простая настройка расписания
+app.conf.beat_schedule = {
+    'block-inactive-users-every-day': {
+        'task': 'users.tasks.block_inactive_users',
+        'schedule': crontab(hour=3, minute=0),  # Каждый день в 3:00 ночи
+    },
+}
