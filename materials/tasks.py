85bb0231e27f2637b286c3b4b67d2c9ecdff3390
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-05 00:02:13 +0500

    add tasks in app materials

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,6 +1,22 @@
 from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+from materials.models import Subscribe
+import logging
 
+logger = logging.getLogger(__name__)
 
 @shared_task
-def add(x, y):
-    return x + y
+def send_course_update_notification(course_id):
+    try:
+        subscriptions = Subscribe.objects.filter(course_id=course_id).select_related("user")
+        for subscription in subscriptions:
+            send_mail(
+                subject='Обновление материалов курса',
+                message='Курс, на который вы подписаны, был обновлен. Проверьте новые материалы!',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[subscription.user.email],
+                fail_silently=False,
+            )
+    except Exception as e:
+        logger.error(f"Error sending course update notifications: {str(e)}")
